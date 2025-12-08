542dc7cb481d50007d18c567c5681702e8ee141e
zoli247 <zoltanpatkovics@gmail.com>
2025-12-08 15:55:17 +0000

    Update info.php

--- Riot-Simulator-Web/info.php
+++ Riot-Simulator-Web/info.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+require_once "protected/config.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,21 +15,36 @@
 
 <body>
   <header>
-    <nav>
-      <a href="index.php">Home</a>
-      <a href="play.php">Play</a>
-      <a href="info.php">Info</a>
-      <a href="credits.php">Credits</a>
+    <nav class="nav-bar">
+
+      <div class="nav-left">
+        <a href="index.php">Home</a>
+        <a href="play.php">Play</a>
+        <a href="info.php">Info</a>
+        <a href="credits.php">Credits</a>
+      </div>
+
+      <?php if (isset($_SESSION['user_id'])): ?>
+        <div class="nav-right">
+          <span>Logged in: <?= htmlspecialchars($_SESSION['username']) ?></span>
+          <a href="logout.php" class="login-btn">Logout</a>
+        </div>
+      <?php else: ?>
+        <div class="nav-right">
+          <a href="login.php" class="login-btn">Login</a>
+        </div>
+      <?php endif; ?>
     </nav>
   </header>
 
   <main>
     <h2>Game Guide</h2>
-    
+
     <section class="content-section">
       <h3>The Prisoner (You)</h3>
       <p>
-        You play as a prisoner who has just escaped from your cell. Your goal is to survive and evade capture by the pursuing police officers.
+        You play as a prisoner who has just escaped from your cell. Your goal is to survive and evade capture by the
+        pursuing police officers.
       </p>
       <h4>Abilities:</h4>
       <ul>
@@ -35,7 +55,8 @@
     <section class="content-section">
       <h3>Police Officer</h3>
       <p>
-        First type of cop you will encounter after you escape. They have similar walkspeed to you and doesn't have any firearms on them
+        First type of cop you will encounter after you escape. They have similar walkspeed to you and doesn't have any
+        firearms on them
       </p>
       <h4>Tips:</h4>
       <ul>
@@ -47,7 +68,8 @@
       <h3>How to Play</h3>
       <h4>Objective:</h4>
       <p>Fight off waves of oncoming police officers. Try to survive as long as you can.</p>
-      <br> <h4>Controls:</h4>
+      <br>
+      <h4>Controls:</h4>
       <table class="controls">
         <tr>
           <td><strong>W</strong></td>
